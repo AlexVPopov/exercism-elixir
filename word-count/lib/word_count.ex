90c5b3e000b45077acc9dfe7bceb9a2ef50175e0
Alexander Popov <alex.val.popov@gmail.com>
2020-01-06 19:07:47 +0200

    Add word count

--- word-count/lib/word_count.ex
+++ word-count/lib/word_count.ex
@@ -6,5 +6,14 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    Regex.replace(~r/[^\w\s\d\-_]+/u, sentence, "")
+    |> String.split(~r/\s+|_/)
+    |> Enum.map(&String.downcase/1)
+    |> Enum.reduce(%{}, fn word, stats ->
+      Map.get_and_update(stats, word, fn current_count ->
+        {current_count, (current_count || 0) + 1}
+      end)
+      |> elem(1)
+    end)
   end
 end
